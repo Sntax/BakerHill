23911a373039decdfdc1206958d4c9884ea1c6f4
Sntax <andrewjrhill@gmail.com>
2015-05-03 15:09:26 +0200

    Initial Commit. Includes file structure and layouts.

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+/* ===================================================== */
+/* Component:                       Global Functionality */
+/* Project Name:                      Baker Hill Digital */
+/* Project URL:                 www.bakerhilldigital.com */
+/* Author(s):       Andrew Hill - andrewjrhill@gmail.com */
+/* ===================================================== */
+
+var globalScope = {
+
+  init: function() {
+  }
+};
+
+globalScope.init();
