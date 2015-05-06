41a00a6a7e90bb4ab3507ede130b1b5a4e2ad210
Sntax <andrewjrhill@gmail.com>
2015-05-06 15:17:33 +0200

    Splash Page layout complete. Includes styles and basic functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,61 @@
 /* Author(s):       Andrew Hill - andrewjrhill@gmail.com */
 /* ===================================================== */
 
-var globalScope = {
+var bakerHill = {
 
   init: function() {
+    bakerHill.logoFade();
+    bakerHill.buttonFade();
+  },
+
+  logoFade: function() {
+
+    var logoText = $('div.logo h1.logo-text');
+    var logoHome = $('div.logo h1.logo-home');
+
+    logoText.mouseover(
+      function() {
+        logoText.animate({
+          left: -250
+        }, 400);
+        logoHome.animate({
+          left: 0,
+          color: '#fff'
+        }, 400);
+      }
+    )
+    logoHome.mouseout(
+      function() {
+        logoText.animate({
+          left: 0
+        }, 400);
+        logoHome.animate({
+          left: 250,
+          color: '#ff0051'
+        }, 400);
+      }
+    )
+  },
+
+  buttonFade: function() {
+    $('.button-light').mouseover(
+      function() {
+        $(this).animate({
+          backgroundColor: '#ff0051',
+          color: '#fff',
+          width: '115px'
+        }, 250);
+      }
+    ).mouseout(
+      function() {
+        $(this).animate({
+          backgroundColor: '#fff',
+          color: '#ff0051',
+          width: '100px'
+        }, 250);
+      }
+    );
   }
 };
 
-globalScope.init();
+bakerHill.init();
